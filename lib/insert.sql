0438a435aed981f20d4b5446ce4c2c377e4f9303
Wambui <nderituwambui93@gmail.com>
2024-06-22 02:22:00 +0300

    updating labs

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 9, 'M', 'brown', 'calm', 'true' );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'F', 'black', 'calm', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 8, 'M', 'yellow', 'calm', 'false');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'F', 'brown', 'calm', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'M', 'green', 'calm', 'false');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'F', 'brown', 'calm', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'M', 'red', 'calm', 'false');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'F', 'brown', 'calm', 'true');
